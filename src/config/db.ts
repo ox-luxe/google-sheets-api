e992eb18998f67ab6be56e238792fc86e183352a
junwensoh <junwen.soh@thecarousell.com>
2023-02-23 00:42:30 +0800

    Create pool mysql

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,11 @@
 import mysql from "mysql2";
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'merchandising',
+  host: process.env.MY_SQL_DB_HOST,
+  user: process.env.MY_SQL_DB_USER,
+  password: process.env.MY_SQL_DB_PASSWORD,
+  database: process.env.MY_SQL_DB_DATABASE,
+  port: parseInt(process.env.MY_SQL_DB_PORT)
 });
 
 export const db = pool.promise();
