d92fb430522869667d54bbd268d12a3e52569fcc
junwensoh <junwen.soh@thecarousell.com>
2023-02-27 11:40:29 +0800

    Refactoring exercise

--- src/helpers/Jotforms.ts
+++ src/helpers/Jotforms.ts
@@ -1,7 +1,10 @@
+import { Merchandiser } from "../models/Merchandiser";
+import { RunningNumberSKU } from "../models/RunningNumberSKU";
+import { Product } from "../models/Product";
 import { getMonthName } from "./getMonthName";
+import { convertNumberToThreeDigitString } from "./convertNumberToThreeDigitString";
+import { sanitiseFormFieldIds } from "./sanitiseFormFieldIds";
 import {
-  ConsignedProducts,
-  AcquiredProducts,
   ConsignmentProduct,
   AcquisitionProduct,
   InboundProducts,
@@ -19,8 +22,7 @@ class Jotform {
 
   constructor(data: Jotform) {
     this.data = data;
-    this.inboundProducts = JSON.parse(this.data.rawRequest);
-    
+    this.inboundProducts = sanitiseFormFieldIds(JSON.parse(this.data.rawRequest));
   }
   identity() {
     if (
@@ -37,44 +39,44 @@ class Jotform {
       return;
     }
   }
-
   getFormSubmissionReference() {
-    return this.inboundProducts.q5_formId5;
+    return this.inboundProducts.formId5;
   }
-
   // methods to extract common product attributes from both forms
-    getId() {
+  getId() {
     return this.inboundProducts.slug.split("/")[1];
   }
   getSellerName() {
-    return this.inboundProducts.q3_sellersName;
+    return this.inboundProducts.sellersName;
   }
-
   getSellerContact() {
-    return this.inboundProducts.q10_sellersContact;
+    return this.inboundProducts.sellersContact;
   }
   getSellerId() {
-    return this.inboundProducts.q9_sellersId;
+    return this.inboundProducts.sellersId;
   }
   getSellerAddress() {
-    return this.inboundProducts.q13_sellersAddress;
+    return this.inboundProducts.sellersAddress;
   }
   getSellerEmail() {
-    return this.inboundProducts.q14_sellersEmail;
+    return this.inboundProducts.sellersEmail;
   }
   getHandoverDate() {
     return (
-      this.inboundProducts.q16_handoverDate.day +
+      this.inboundProducts.handoverDate.day +
       "-" +
-      getMonthName(this.inboundProducts.q16_handoverDate.month) +
+      getMonthName(this.inboundProducts.handoverDate.month) +
       "-" +
-      this.inboundProducts.q16_handoverDate.year
+      this.inboundProducts.handoverDate.year
     );
   }
   getProductDetails() {
-    return JSON.parse(this.inboundProducts.q8_test);
+    return JSON.parse(this.inboundProducts.test);
   }
-  getProductAdditionalItemsStatus(prod: ConsignmentProduct | AcquisitionProduct) {
+  getProductAdditionalItemsStatus(
+    prod: ConsignmentProduct | AcquisitionProduct
+  ) {
+    // item order is important and must match google sheet item order
     const POSSIBLE_ADDITIONAL_ITEMS = [
       "Authenticity Card",
       "Care Card",
@@ -96,49 +98,29 @@ class Jotform {
     });
     return status;
   }
-
-  addCalculatedCellsInProductRows(productsWithConsignmentDetails: string[][], lastUsedRow: number) {
-    let newRow = lastUsedRow + 1;
-    let productRows = [...productsWithConsignmentDetails];
-    for (let row = 0; row < productsWithConsignmentDetails.length; row++) {
-      productRows[row][3] = `=IF(C${newRow}="","",DATEDIF(C${newRow},TODAY(), "d"))`;
-      productRows[row][7] = `=J${newRow}&" "&K${newRow}`;
-      productRows[row][18] = `=(Q${newRow}-R${newRow})/Q${newRow}`;
-      productRows[row][20] = `=Q${newRow}/P${newRow}`;
-      newRow++;
+  getProductAdditionalItemsList(additionalItemsStatus: string[]) {
+    let list = "";
+    const POSSIBLE_ADDITIONAL_ITEMS = [
+      "Authenticity Card",
+      "Care Card",
+      "Dust Bag",
+      "Original Box",
+      "Paper Bag",
+      "Receipt(s)",
+    ];
+    for (let i = 0; i < POSSIBLE_ADDITIONAL_ITEMS.length; i++) {
+      if (additionalItemsStatus[i] === "Y") {
+        list += POSSIBLE_ADDITIONAL_ITEMS[i] + " | ";
+      }
     }
-    
-    return productRows;
+    return list.substring(0, list.length - 3); // housekeeping: remove the last " | "
   }
-}
-
-class AcquisitionForm extends Jotform {
-  data: Jotform;
-  acquiredProducts: AcquiredProducts;
-
-  constructor(data: Jotform) {
-    super(data);
-    this.acquiredProducts = JSON.parse(this.data.rawRequest);
-  }
-
   getSellerReachOut() {
-    return this.acquiredProducts.q74_sellerReach;
+    return this.inboundProducts.sellerReach;
   }
-
-  getProductSku() {
-    // q16_handoverDate: { day: '01', month: '01', year: '2023' }
-    return (
-      "S" +
-      this.acquiredProducts.q16_handoverDate.year.slice(2, 4) +
-      this.acquiredProducts.q16_handoverDate.month +
-      "XXX" +
-      this.getVendorNameInitials()
-    );
-  }
-
   getVendorNameInitials() {
-    const vendorName = this.acquiredProducts.q86_vendorsName.split(" ");
-
+    const vendorName = this.inboundProducts.vendorsName.split(" ");
+    
     if (vendorName.length > 1) {
       return (
         vendorName[0].slice(0, 1).toUpperCase() +
@@ -150,22 +132,94 @@ class AcquisitionForm extends Jotform {
       return "";
     }
   }
+  addCalculatedCellsInProductRows(
+    productsWithConsignmentDetails: string[][],
+    lastUsedRow: number
+  ) {
+    let newRow = lastUsedRow + 1;
+    let productRows = [...productsWithConsignmentDetails];
+    for (let row = 0; row < productsWithConsignmentDetails.length; row++) {
+      productRows[
+        row
+      ][3] = `=IF(C${newRow}="","",DATEDIF(C${newRow},TODAY(), "d"))`;
+      productRows[row][7] = `=J${newRow}&" "&K${newRow}`;
+      productRows[row][18] = `=(Q${newRow}-R${newRow})/Q${newRow}`;
+      productRows[row][20] = `=Q${newRow}/P${newRow}`;
+      newRow++;
+    }
 
-  getProductsWithAcquisitionDetails() {
+    return productRows;
+  }
+  async getSkuRunningNumber(merchandiserName: string) {
+    try {
+      const merch = new Merchandiser(merchandiserName);
+      const { id } = await merch.findInfo();
+
+      const rn = new RunningNumberSKU();
+      const currentRunningNumberDetailsOfMerch =
+        await rn.queryRunningNumberOfMerchandiser(id);
+
+      if (currentRunningNumberDetailsOfMerch) {
+        return currentRunningNumberDetailsOfMerch.running_number;
+      } else {
+        rn.intializeRunningNumberForMerchandiser(id);
+        return 1;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async generateProductSku() {
+    const vendorName = this.inboundProducts.vendorsName;
+    const skuRunningNumber: number = await this.getSkuRunningNumber(vendorName);
+
+    return (
+      (this.identity() === "Acquisition" ? "S" : "C") +
+      this.inboundProducts.handoverDate.year.slice(2, 4) +
+      this.inboundProducts.handoverDate.month +
+      convertNumberToThreeDigitString(skuRunningNumber) +
+      this.getVendorNameInitials()
+    );
+  }
+  async incrementRunningNumberOfSku(merchandiserName: string) {
+    const merch = new Merchandiser(merchandiserName);
+    const { id } = await merch.findInfo();
+    const rn = new RunningNumberSKU();
+    await rn.incrementRunningNumberOfSKU(id);
+  };
+  async getMerchandiserId(merchandiserName: string) {
+    const merch = new Merchandiser(merchandiserName);
+    const { id } = await merch.findInfo();
+    return id;
+  };
+}
+
+class AcquisitionForm extends Jotform {
+  data: Jotform;
+
+  constructor(data: Jotform) {
+    super(data);
+  }
+
+  async getProductsWithAcquisitionDetails() {
     // structure of returned data is deliberately made to suit input format
     // for gsheet bulk update operation e.g. string[][]
-
     const productDetails = this.getProductDetails();
-    return productDetails.map((prod: AcquisitionProduct) => {
-      // empty strings below represent cells in google sheet that don't required automated inputs 
-      return [
+    let productsWithAcquisitionDetails: string[][] = [];
+
+    for (const prod of productDetails) {
+      const productSKU = await this.generateProductSku();
+      const availableAdditionalItemsStatus =
+        this.getProductAdditionalItemsStatus(prod);
+
+      const productWithAcquisitionDetails = [
         "A",
         this.getSellerReachOut(),
         this.getHandoverDate(),
         "",
         "",
         this.getFormSubmissionReference(),
-        this.getProductSku(),
+        productSKU,
         "",
         prod.Brand,
         prod.Model,
@@ -187,66 +241,64 @@ class AcquisitionForm extends Jotform {
         prod["Product s/n"],
         "",
         "",
-      ].concat(this.getProductAdditionalItemsStatus(prod));
-    });
+      ]
+        .concat(availableAdditionalItemsStatus)
+        .concat([
+          `-SKU: ${productSKU}
+        -Material: ${prod.Material} 
+        -Color: ${prod.Color} 
+        -Hardware: 
+        -Interior: 
+        -Exterior: 
+        -Attached:
+        -Dimension:
+        -Receipt: ${availableAdditionalItemsStatus[5] === "Y" ? "Yes" : "No"}
+        -Accessories: ${this.getProductAdditionalItemsList(
+          availableAdditionalItemsStatus
+        )}
+        -Date code / Blind Stamp / Series: 
+        -Made in: `,
+        ]);
+      productsWithAcquisitionDetails.push(productWithAcquisitionDetails);
+
+      // After generating a SKU number for each product, we should increment
+      // the running number of the Merchandiser to keep each SKU unique.
+      await this.incrementRunningNumberOfSku(this.inboundProducts.vendorsName);
+
+      // Save a copy of the product details with the generated SKU in db for future reference
+      const merchId = await this.getMerchandiserId(this.inboundProducts.vendorsName);
+      const product = new Product(merchId, "A", productSKU, prod.Brand, prod.Model, prod.Color, prod["Product s/n"]);
+      await product.save();
+    }
+    return productsWithAcquisitionDetails;
   }
-
-
 }
-
 class ConsignmentForm extends Jotform {
   data: Jotform;
-  consignedProducts: ConsignedProducts;
+
   constructor(data: Jotform) {
     super(data);
-    this.consignedProducts = JSON.parse(this.data.rawRequest);
   }
 
-  getSellerReachOut() {
-    return this.consignedProducts.q73_sellerReach;
-  }
-
-  getProductSku() {
-    // q16_handoverDate: { day: '01', month: '01', year: '2023' }
-    return (
-      "C" +
-      this.consignedProducts.q16_handoverDate.year.slice(2, 4) +
-      this.consignedProducts.q16_handoverDate.month +
-      "XXX" +
-      this.getVendorNameInitials()
-    );
-  }
-  
-  getVendorNameInitials() {
-    const vendorName = this.consignedProducts.q22_vendorsName.split(" ");
-
-    if (vendorName.length > 1) {
-      return (
-        vendorName[0].slice(0, 1).toUpperCase() +
-        vendorName[1].slice(0, 1).toUpperCase()
-      );
-    } else if (vendorName.length === 1) {
-      return vendorName[0].slice(0, 1).toUpperCase();
-    } else {
-      return "";
-    }
-  }
-
-  getProductsWithConsignmentDetails() {
+  async getProductsWithConsignmentDetails() {
     // structure of returned data is deliberately made to suit input format
     // for gsheet bulk update operation e.g. string[][]
 
     const productDetails = this.getProductDetails();
-    return productDetails.map((prod: ConsignmentProduct) => {
-      // empty strings below represent cells in google sheet that don't required automated inputs 
-      return [
+    let productsWithConsignmentDetails: string[][] = [];
+
+    for (const prod of productDetails) {
+      const productSKU = await this.generateProductSku();
+      const availableAdditionalItemsStatus = this.getProductAdditionalItemsStatus(prod);
+
+      const productWithConsignmentnDetails = [
         "C",
         this.getSellerReachOut(),
         this.getHandoverDate(),
         "",
         "",
         this.getFormSubmissionReference(),
-        this.getProductSku(),
+        productSKU,
         "",
         prod.Brand,
         prod.Model,
@@ -268,14 +320,36 @@ class ConsignmentForm extends Jotform {
         prod["Product s/n"],
         "",
         "",
-      ].concat(this.getProductAdditionalItemsStatus(prod));
-    });
+      ]
+        .concat(availableAdditionalItemsStatus)
+        .concat([
+          `-SKU: ${productSKU}
+        -Material: ${prod.Material} 
+        -Color: ${prod.Color} 
+        -Hardware: 
+        -Interior: 
+        -Exterior: 
+        -Attached:
+        -Dimension:
+        -Receipt: ${availableAdditionalItemsStatus[5] === "Y" ? "Yes" : "No"}
+        -Accessories: ${this.getProductAdditionalItemsList(
+          availableAdditionalItemsStatus
+        )}
+        -Date code / Blind Stamp / Series:
+        -Made in: `,
+        ]);
+      productsWithConsignmentDetails.push(productWithConsignmentnDetails);
+
+      // After generating a SKU number for each product, we should increment
+      // the running number of the Merchandiser to keep each SKU unique.
+      await this.incrementRunningNumberOfSku(this.inboundProducts.vendorsName);
+      // Save a copy of the product details with the generated SKU in db for future reference
+      const merchId = await this.getMerchandiserId(this.inboundProducts.vendorsName);
+      const product = new Product(merchId, "C", productSKU, prod.Brand, prod.Model, prod.Color, prod["Product s/n"]);
+      await product.save();
+    }
+    return productsWithConsignmentDetails;
   }
-
-
 }
 
-export { 
-  AcquisitionForm, 
-  ConsignmentForm, 
-  Jotform };
+export { AcquisitionForm, ConsignmentForm, Jotform };
